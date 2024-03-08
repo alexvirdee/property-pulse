e4a906da81e6b352fdbded41393e87c13ce96afb
Alex Virdee <alex.virdee08@gmail.com>
2024-03-08 16:29:24 -0500

    file routing

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-
+import Link from "next/link";
 
 const HomePage = () => {
   return (
-    <div>Home page</div>
+    <div>
+      <h1 className="text-3xl">Welcome</h1>
+      <Link href='/properties'>Show Properties</Link>
+    </div>
   )
 }
 
-export default HomePage
+export default HomePage;
