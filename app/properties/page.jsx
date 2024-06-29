6aaf9fb49236d41e96dac90f0d86d11e1618ea80
Alex Virdee <alex.virdee08@gmail.com>
2024-06-29 11:03:49 -0400

    using cloudinary for images

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,7 +1,6 @@
 import PropertyCard from '@/components/PropertyCard';
 import { fetchProperties } from '@/utils/requests';
 
-
 const PropertiesPage = async () => {
   const properties = await fetchProperties();
 
